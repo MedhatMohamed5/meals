292ebf5d41e31cd8a9851b125a7c079ebc5d21b7
Medhat Mohamed <35978271+MedhatMohamed5@users.noreply.github.com>
2020-11-21 15:42:02 +0200

    Adding TabBar, Styling meal details Till Lesson 171

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
@@ -7,18 +8,100 @@ class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => mealId == meal.id);
 
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '$mealId',
+          '${selectedMeal.title}',
         ),
       ),
-      body: Center(
-        child: Text(
-          'The meal - $mealId',
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedMeal.imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            buildSectionTitle(context, 'Ingredients'),
+            buildContainer(
+              context,
+              ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    color: Theme.of(context).accentColor,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8.0,
+                        horizontal: 4,
+                      ),
+                      child: Text(
+                        selectedMeal.ingredients[index],
+                      ),
+                    ),
+                  );
+                },
+                itemCount: selectedMeal.ingredients.length,
+              ),
+            ),
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+              context,
+              ListView.builder(
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text(
+                          '# ${index + 1}',
+                        ),
+                      ),
+                      title: Text(
+                        selectedMeal.steps[index],
+                      ),
+                    ),
+                    if (index < selectedMeal.steps.length - 1) Divider(),
+                  ],
+                ),
+                itemCount: selectedMeal.steps.length,
+              ),
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget buildSectionTitle(BuildContext context, String title) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Text(
+        title,
+        style: Theme.of(context).textTheme.headline6,
+      ),
+    );
+  }
+
+  Widget buildContainer(BuildContext context, Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
+      height: 150,
+      width: MediaQuery.of(context).size.width * 0.80,
+      child: child,
     );
   }
 }
